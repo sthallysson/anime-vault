d86ec285dfa1f7973391851ef57d3653c66a9fc9
sthallysson <sthallysson5@gmail.com>
2023-12-15 21:21:07 -0300

    feat: handling errors

--- src/app/action.tsx
+++ src/app/action.tsx
@@ -1,18 +1,20 @@
 "use server";
 import axios from "axios";
 import AnimeCard, { AnimeProp } from "../components/AnimeCard";
-import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
 
 // Todo codigo escrito nesse arquivo será executado como uma Server Action
 
 export const fecthAnime = async (page: number) => {
   // Fazer chamada para API e retornar os dados
+  try {
+    const response = await axios.get(
+      `https://shikimori.one/api/animes?page=${page}&limit=8&order=popularity`
+    );
 
-  const response = await axios.get(
-    `https://shikimori.one/api/animes?page=${page}&limit=8&order=popularity`
-  );
-
-  return response.data.map((item: AnimeProp, index: number) => (
-    <AnimeCard key={item.id} anime={item} index={index} />
-  ));
+    return response.data.map((item: AnimeProp, index: number) => (
+      <AnimeCard key={item.id} anime={item} index={index} />
+    ));
+  } catch (error) {
+    return <h2 className="text-3xl font-extrabold text-red-500">Deu Erro</h2>;
+  }
 };
